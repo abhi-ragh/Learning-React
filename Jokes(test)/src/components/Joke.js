89156a985d4ce901dd3df3114a0ec766c871a766
abhi-ragh <abhiragh0@gmail.com>
2024-07-15 15:37:52 +0530

    jokes - messing around with event listeners

--- Jokes(test)/src/components/Joke.js
+++ Jokes(test)/src/components/Joke.js
@@ -1,4 +1,7 @@
 export default function Joke(props){
+    function handleClick(){
+        console.log("AAAAAAAAAAAsdsdsdsdsdAAAAAAa")
+    }
     return(
         <>
         {props.setup && <p style={{fontSize:"30px", fontFamily:"Bold"}}>{props.setup}</p>}
@@ -6,6 +9,10 @@ export default function Joke(props){
         {props.setup && <p style={{fontSize:"20px", fontFamily:"cursive"}}>{props.punchline}</p>}
         <h3>Upvotes: {props.upvotes}</h3>
         < hr />
+
+
+        <img src="pfp.jpg" className="pfp" onMouseEnter={handleClick}/>
+        <button onClick={handleClick}>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</button>
         </>
     )
 }
