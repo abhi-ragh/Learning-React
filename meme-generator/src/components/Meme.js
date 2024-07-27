82307601c699ddf081f93447fb14c11be39d7878
abhi-ragh <abhiragh0@gmail.com>
2024-07-27 22:23:00 +0530

    meme generator complete

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,5 +1,4 @@
-import MemesData from "../MemesData"
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 export default function Meme(){
     let url;
@@ -9,7 +8,13 @@ export default function Meme(){
         randomImage: "https://i.imgflip.com/39t1o.jpg"
     });
 
-    const [allMemeImages,setAllMemeImages] = useState(MemesData)
+    const [allMemeImages,setAllMemeImages] = useState([])
+
+    useEffect( () => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemeImages(data.data.memes))
+    }, [])
 
     function HandleClick(){
         url = allMemeImages[Math.floor(Math.random() * allMemeImages.length)].url;
@@ -20,20 +25,47 @@ export default function Meme(){
             }
         });
     }
+
+    function handleInput(event){
+        setMeme(prevState => {
+            return{
+                ...prevState,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
     return(
         <div className="form">
             <div className="form--top">
             <section className="form--top-text">
                 <p className="form--input-text">Top Text</p>
-                <input placeholder="Shut Up"/>
+                <input 
+                    type="text"
+                    placeholder="Shut Up"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleInput}
+                />
             </section>
             <section className="form--down-text">
                 <p className="form--input-text">Down Text</p>
-                <input placeholder="and take my money"/>
+                <input
+                    type="text"
+                    placeholder="and take my money"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleInput}    
+                />
             </section>
             </div>
             <button className="form--submit" onClick={HandleClick}>Get a new meme image 🖼 </button>
-            <img src={meme.randomImage} className="form-meme"/>
+
+            <div className="meme--area">
+                <h1 className="meme--text" id="topText">{meme.topText}</h1>
+                <img src={meme.randomImage} className="form-meme"/>
+                <h1 className="meme--text" id="bottomText">{meme.bottomText}</h1>
+            </div>
         </div>
     )
 }
